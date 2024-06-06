79b55ba87daad3116d583034de738b82247d4e96
ASUS\mahbu <hasanmahbub331@gmail.com>
2024-06-06 23:34:34 +0600

    RegionsController Added.

---
+++ Postman/Postman.API/Controllers/RegionsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Postman.API.Model.Domain;
+
+namespace Postman.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        // GET BY: api/Regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Sylhet Region",
+                    Code = "Syl",
+                    RegionImgUrl = "https://www.tbsnews.net/sites/default/files/styles/amp_metadata_content_image_min_696px_wide/public/images/2022/01/28/sylhet_talha-chowdhury.jpg"
+
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Dhaka Region",
+                    Code = "Dhk",
+                    RegionImgUrl = "https://www.gettyimages.com/photos/dhaka"
+                }
+
+
+            };
+
+            return Ok(regions);
+        }
+    }
+}
--- Postman/Postman.API/obj/Debug/net8.0/Postman.API.AssemblyInfo.cs
+++ Postman/Postman.API/obj/Debug/net8.0/Postman.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Postman.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a676dd564e06d3192785c78b45a8ddafb908c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75069130f691ee935aea3ddd586c507bd254866a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Postman.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Postman.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
