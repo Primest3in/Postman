416942d251745931f75a79585f6d1847b27555de
ASUS\mahbu <hasanmahbub331@gmail.com>
2024-06-07 22:57:06 +0600

    DTO added for Region model

---
+++ Postman/Postman.API/Model/DTO/RegionDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Postman.API.Model.DTO
+{
+    public class Class
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? RegionImgUrl { get; set; }
+    }
+}
