0e8f2acb7a28c322bd6a9ed5b3f8a7ac510f58e3
ASUS\mahbu <hasanmahbub331@gmail.com>
2024-06-14 11:53:00 +0600

    miscellaneous

--- Postman/Postman.API/obj/Debug/net8.0/Postman.API.AssemblyInfo.cs
+++ Postman/Postman.API/obj/Debug/net8.0/Postman.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Postman.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75069130f691ee935aea3ddd586c507bd254866a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771e767b11156bd9dbf668dadeb3f359e0d3766b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Postman.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Postman.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
