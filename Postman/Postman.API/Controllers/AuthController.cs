e951587381f3546f84a0fe9305b66f2355df684f
ASUS\mahbu <hasanmahbub331@gmail.com>
2024-06-19 20:46:25 +0600

    AuthController added and user can be registered now

---
+++ Postman/Postman.API/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Postman.API.Model.DTO;
+
+namespace Postman.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDTO.UserName,
+                Email = registerRequestDTO.UserName
+            };
+            var identityResult =  await userManager.CreateAsync(identityUser, registerRequestDTO.Password);
+            if(identityResult.Succeeded)
+            {
+                if(registerRequestDTO.Roles != null) 
+                { 
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDTO.Roles);
+                    if(identityResult.Succeeded)
+                    {
+                        return Ok("User Created");
+                    }
+                }
+            }
+
+            return Ok("Something went wrong");
+        }
+    }
+}
